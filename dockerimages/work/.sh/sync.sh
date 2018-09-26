53cc9cff0be7200005cb9e6a50c9528ca8ba5e34
luuil <luuil@outlook.com>
2018-09-26 15:45:11 +0800

    1. sync.sh more readablity;\n2. more details in README

--- dockerimages/work/.sh/sync.sh
+++ dockerimages/work/.sh/sync.sh
@@ -2,18 +2,18 @@
 
 function sync() {
   printf "[$1]: syncing...\n"
-  rsync --exclude='~/.sh/sync.sh' -r ~/.sh/ $1:~/.sh
-  alter_or_not=`ssh $1 "cat ~/.bashrc | grep bashrc_append"`
+  rsync -azP --exclude='~/.sh/sync.sh' ~/.sh/ $1:~/.sh
+  alter_or_not=`ssh -t $1 "cat ~/.bashrc | grep bashrc_append"`
   if [ -n "$alter_or_not" ]; then
     printf "[$1]: synced already\n\n"
   else
     printf "[$1]: append 'source ~/.sh/.bashrc_append' to '~/.bashrc'\n\n"
-    ssh $1  "echo ""source /home/\`whoami\`/.sh/.bashrc_append"" >> ~/.bashrc"
+    ssh -t $1 "sed -i '\$a # my bashrc\nsource ~/.sh/.bashrc_append' ~/.bashrc"
   fi
 }
 
-ssh-copy-id -f -i ~/.ssh/id_rsa.pub ti1
-ssh-copy-id -f -i ~/.ssh/id_rsa.pub ti2
+ssh-copy-id -i ~/.ssh/id_rsa.pub ti1
+ssh-copy-id -i ~/.ssh/id_rsa.pub ti2
 
 sync nl1
 sync nl2
