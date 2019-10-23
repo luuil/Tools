59b81a5b6ffb404b60e90e2a2264620a580ee4b5
liulu <luuil@outlook.com>
2019-10-23 19:23:45 +0800

    pylogging: using envs to config

--- pylogging/test.py
+++ pylogging/test.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # author: luuil@outlook.com
 # created: 2019-10-23 17:57:20
-# modified: 2019-10-23 18:44:15
+# modified: 2019-10-23 19:22:35
 # =============================================================
 r"""Test logging."""
 
@@ -13,14 +13,15 @@ from core.utils import envs
 if __name__ == "__main__":
   import os
 
-  # os.environ["LOG_CFG"] = "configs/logger_example.yaml"
-  # os.environ["LOG_NAME"] = "example"
+  os.environ["LOG_CFG"] = "configs/logger_example.yaml"
+  os.environ["LOG_NAME"] = "example"
 
   os.makedirs("./logs", exist_ok=True)
 
-  setup_logging()
-
-  logger = get_logger("example")
+  log_name, log_cfg = envs("LOG_NAME", "LOG_CFG")
+  
+  setup_logging(log_cfg)
+  logger = get_logger(log_name)
 
   logger.debug("debug")
   logger.info("info")
